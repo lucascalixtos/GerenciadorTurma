f65e2a7e4f7939d933a2767e173ae5975027c36b
Lucas <lucascalixto180@gmail.com>
2024-04-21 19:58:37 -0300

    test

---
+++ GerenciadorTurma.FrontEnd/GerenciadorTurma.FrontEnd/Controllers/AlunosController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GerenciadorTurma.FrontEnd.Data;
+using GerenciadorTurma.FrontEnd.Models;
+using GerenciadorTurma.FrontEnd.Models.Interfaces;
+using System.Text.RegularExpressions;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GerenciadorTurma.FrontEnd.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly IAlunoService _service;
+
+        public AlunosController(IAlunoService alunoService)
+        {
+            _service = alunoService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.BuscarTodosAlunos());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _service.BuscarAluno(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Alunos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,Usuario,senha")] Aluno aluno)
+        {
+           if (ModelState.IsValid)
+            {
+                if (!ValidarSenhaAluno(aluno))
+                {
+                    ViewBag.MensagemErro = "A senha não atende aos critérios de segurança.";
+                    return View();
+                }
+
+                if (aluno.Nome.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo Nome";
+                    return View();
+                }
+
+                if (aluno.Usuario.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo Usuario";
+                    return View();
+                }
+
+                await _service.CriarAluno(aluno);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _service.BuscarAluno(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("Nome,Usuario,senha, Id")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                if (aluno.Nome.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo Nome";
+                    return View();
+                }
+
+                if (aluno.Usuario.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo Usuario";
+                    return View();
+                }
+
+                await _service.EditarAluno(aluno);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _service.ApagarAluno(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("index");
+        }
+
+        public bool ValidarSenhaAluno(Aluno aluno)
+        {
+            if (aluno.senha.Length < 8)
+                return false;
+
+            if (!Regex.IsMatch(aluno.senha, @"\d"))
+                return false;
+
+            if (!Regex.IsMatch(aluno.senha, @"[!@#$%^&*()_+{}\[\]:;<>,.?~]"))
+                return false;
+
+            if (!Regex.IsMatch(aluno.senha, @"[A-Z]"))
+                return false;
+
+            if (!Regex.IsMatch(aluno.senha, @"[a-z]"))
+                return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ GerenciadorTurma.FrontEnd/GerenciadorTurma.FrontEnd/Controllers/TurmasController.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GerenciadorTurma.FrontEnd.Data;
+using GerenciadorTurma.FrontEnd.Models;
+using GerenciadorTurma.FrontEnd.Models.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GerenciadorTurma.FrontEnd.Controllers
+{
+    public class TurmasController : Controller
+    {
+        private readonly ITurmaService _service;
+
+        public TurmasController(ITurmaService TurmaService)
+        {
+            _service = TurmaService;
+        }
+
+        // GET: Turmas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.BuscarTodosTurmas());
+        }
+
+        // GET: Turmas/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.BuscarTurma(id);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(Turma);
+        }
+
+        // GET: Turmas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Turmas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Curso_id,turma,Ano")] Turma turm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (turm.Curso_id <= 0)
+                {
+                    ViewBag.MensagemErro = "O id deve ser maior que 0";
+                    return View();
+                }
+
+                if (turm.turma.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo turma";
+                    return View();
+                }
+
+                if (turm.Ano < 1000 || turm.Ano > 9999)
+                {
+                    ViewBag.MensagemErro = "Digite um ano com 4 dígitos";
+                    return View();
+                }
+
+                if (turm.Ano < DateTime.Now.Year)
+                {
+                    ViewBag.MensagemErro = "Digite um ano que seja maior ou igual o atual";
+                    return View();
+                }
+
+                await _service.CriarTurma(turm);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turm);
+        }
+
+        // GET: Turmas/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.BuscarTurma(id);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+            return View(Turma);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("Curso_id,turma,Ano,Id")] Turma turm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (turm.Curso_id <= 0)
+                {
+                    ViewBag.MensagemErro = "O id deve ser maior que 0";
+                    return View();
+                }
+
+                if (turm.turma.IsNullOrEmpty())
+                {
+                    ViewBag.MensagemErro = "Preencha o campo turma";
+                    return View();
+                }
+
+                if (turm.Ano < 1000 || turm.Ano > 9999)
+                {
+                    ViewBag.MensagemErro = "Digite um ano com 4 dígitos";
+                    return View();
+                }
+
+                if (turm.Ano < DateTime.Now.Year)
+                {
+                    ViewBag.MensagemErro = "Digite um ano que seja maior ou igual o atual";
+                    return View();
+                }
+
+
+
+                await _service.EditarTurma(turm);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turm);
+        }
+
+
+        // POST: Turmas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+
+        // GET: Turmas/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.ApagarTurma(id);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("index");
+        }
+
+        public async Task<IActionResult> ListaAlunosTurma(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.BuscarAlunosEmTurma(id);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+            return View(Turma);
+        }
+
+        public async Task<IActionResult> RemoverAlunoDeTurma(int idAluno, int idTurma)
+        {
+            if (idAluno == null && idTurma == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.RemoverAlunoDeTurma(idAluno, idTurma);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("ListaAlunosTurma", new { id = idTurma });
+        }
+
+        public async Task<IActionResult> AdicionarAlunoaTurma(int idAluno, int idTurma)
+        {
+            if (idAluno == null && idTurma == null)
+            {
+                return NotFound();
+            }
+
+            var Turma = await _service.AdicionarAlunoaTurma(idAluno, idTurma);
+            if (Turma == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("ListaAlunosTurma",  new {id = idTurma});
+        }
+
+    }
+}
